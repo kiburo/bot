d4357203b64aba8b913d6eadf3b7c3077509ab1f
kiburo <kiburo8899@gmail.com>
2025-11-15 13:47:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
-# Настройки базы данных PostgreSQL
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 # Настройки для БаЦзы
